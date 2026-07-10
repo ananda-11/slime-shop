f3430bd428e4280bf5385396f5ad36da39edf16d
christyding1 <xiadizhou99@gmail.com>
2026-07-10 12:41:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -42,11 +42,11 @@
 
         <div class="box-section">
             <nav class="navbar" style="width: 1000px;">
-                    <a class="navbar-brand" href="index.php">Slimy shop</a>
+                    <a class="navbar-brand" href="index.html">Slimy shop</a>
                 <div>
-                    <button onclick="window.location.href='calendar.php'">Calendar</button>
-                    <button onclick="window.location.href='gallery.php'">Moments</button>
-                    <button id="login-btn" onclick="window.location.href='signin.php'">Sign in/up</button>
+                    <button onclick="window.location.href='calendar.html'">Calendar</button>
+                    <button onclick="window.location.href='gallery.html'">Moments</button>
+                    <button id="login-btn" onclick="window.location.href='signin.html'">Sign in/up</button>
                 </div>
             </nav>
 
